cfebf5be7858b1495f22093e12a0190577530df0
umarazam <umarazam01@gmail.com>
2019-04-29 22:00:51 +0500

    This is full working k-mean algorithm. Run.py has very critical mistake in it.

--- k-mean.py
+++ k-mean.py
@@ -36,9 +36,8 @@ def k_mean(l1, k):
 def main():
     l1 = [(1.1,1,1),(1.5, 2.1),(3.1,4.1), (5.1, 7.1) ,(3.5,5.1), (4.5,5.1), (3.5,4.5)]
     cluster1, cluster2 = k_mean(l1,2)
-    print(cluster1)
-    print('--------')
-    print(cluster2)                            
+    print('Cluster1: \n',cluster1)
+    print('Cluster2: \n',cluster2)
    
 if __name__ == '__main__':
     main();
